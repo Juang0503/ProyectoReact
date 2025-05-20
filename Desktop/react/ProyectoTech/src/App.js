311135dd1d3b417a502e46628baace93a00d8619
Juan Cardona <juan.cardona38846@ucaldas.edu.co>
2025-05-19 20:41:14 -0500

    final

--- Desktop/react/ProyectoTech/src/App.js
+++ Desktop/react/ProyectoTech/src/App.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DashboardRenovable from './DashboardRenovable';
-import FormularioEstimacion from './FormularioEstimacion';
-import RequestPaises from './RequestPaises';
-import TablaDatosHistoricos from './TablaDatosHistoricos';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardRenovable from "./DashboardRenovable";
+import FormularioEstimacion from "./FormularioEstimacion";
+import RequestPaises from "./RequestPaises";
+import TablaDatosHistoricos from "./TablaDatosHistoricos";
+import Loader from "./Loader"; // Importa la animación de carga
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [mostrarTabla, setMostrarTabla] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // 🔥 Nueva estructura: Si está cargando, solo muestra el Loader
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <Router>
       <div className="container">
         <h2
-  className="my-5 text-center"
-  style={{
-    alignItems: "center",
-    color: "rgb(232, 232, 232)",
-    textShadow: "1px 1px 3px rgb(0, 0, 0)"
-  }}>
-  Monitoreo Renovable
-</h2>
-
-        
-
+          className="my-5 text-center"
+          style={{
+            alignItems: "center",
+            color: "rgb(232, 232, 232)",
+            textShadow: "1px 1px 3px rgb(0, 0, 0)",
+          }}
+        >
+          Monitoreo Renovable
+        </h2>
         <Routes>
           <Route
             path="/"
@@ -30,15 +43,14 @@ function App() {
               <>
                 <DashboardRenovable />
                 <button
-          className="btn btn-success mb-5"
-          onClick={() => setMostrarTabla(!mostrarTabla)}
-        >
-          {mostrarTabla ? 'Ocultar Datos Históricos' : 'Mostrar Datos Históricos'}
-        </button>
+                  className="btn btn-success mb-5"
+                  onClick={() => setMostrarTabla(!mostrarTabla)}
+                >
+                  {mostrarTabla ? "Ocultar Datos Históricos" : "Mostrar Datos Históricos"}
+                </button>
                 {mostrarTabla && <TablaDatosHistoricos />}
                 <FormularioEstimacion />
                 <RequestPaises />
-                
               </>
             }
           />
